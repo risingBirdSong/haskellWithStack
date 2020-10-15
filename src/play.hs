d0289cae554333fab5988eb1fc5ddb00129bdc35
risingbirdsong <heregoingthere@outlook.com>
2020-10-14 23:28:39 -0700

    it works with normal pure, without getting an explicit return type (like im doing in purify with Int)

--- src/play.hs
+++ src/play.hs
@@ -13,5 +13,9 @@ purify x = pure x
 -- ah, useful example of id, the recursive condition can result in an empty array and that composes well with purify
 -- [] <> (purify 3) <> (purify 4)
 
+-- ah interesting, it works with just calling pure on x, I didn't think this would work at first
+-- because when I run pure in the console on a num, I don't get [num], I just get num, which i didn't think 
+-- would append without that structure, but clearly it does work like that.
+rev' :: [a] -> [a]
 rev' [] = []
-rev' (x:xs) = (rev' xs) <> (purify x)
+rev' (x:xs) = (rev' xs) <> (pure x)
