6fdff85f88492bfbab4cb0f1d029f9349b2dcabd
risingbirdsong <heregoingthere@outlook.com>
2020-10-14 23:25:15 -0700

    nice example of purity, id and recurions together

--- src/play.hs
+++ src/play.hs
@@ -2,6 +2,13 @@ import Data.List.Split
 -- reverse without built in 
 split1 xs = chunksOf 1 xs
 c = split1 "abcdefg"
-rev [] = []
-rev (x:xs) =  rev xs ++ x
+-- rev' x = [x]
+-- rev' (x:xs) =  (rev' xs) <> ([x])   
 
+purify :: Int -> [Int]
+purify x = pure x
+
+-- rev' 
+
+-- ah, useful example of id, the recursive condition can result in an empty array and that composes well with purify
+-- [] <> (purify 3) <> (purify 4)
